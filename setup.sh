758153be39b40ffcd98656e104aaa5ff8239db37
halsn <xuhalsn@gmail.com>
2017-05-06 21:37:28 -0700

    update

--- setup.sh
+++ setup.sh
@@ -3,57 +3,97 @@
 sudo apt-get update
 
 #必备软件
-sudo apt-get install -y git ibus-rime ppa-purge tree time curl wget gawk wordnet entr
+install_first() {
+  sudo apt-get install -y git ibus-rime ppa-purge tree time curl wget gawk wordnet entr inotify-tools
+}
+
+config_git() {
+  echo "---------------git---------------"
+  ssh-keygen -t rsa -b 4096 -C "xuhalsn@gmail.com"
+  curl -u "halsn" \
+    --data "{\"title\":\"VM_`date +%Y%m%d%H%M%S`\",\"key\":\"`cat ~/.ssh/id_rsa.pub`\"}" \
+    https://api.github.com/user/keys
+  git config --global user.email "xuhalsn@gmail"
+  git config --global user.name "halsn"
+  echo "------------finished-------------"
+}
+
+config_ubuntu() {
+  echo "--------------clone ubuntu-config---------------"
+  cd $HOME
+  git clone git@github.com:halsn/ubuntu-config && cd ubuntu-config
+  cp -a ./dotfiles/* $HOME
+  cp -a ./App $HOME
+  sudo cp -a ./config/rc.local /etc/
+  echo "-------------------finished---------------------"
+}
 
 #Lantern
-wget https://raw.githubusercontent.com/getlantern/lantern-binaries/master/lantern-installer-beta-64-bit.deb -O lantern.deb
-sudo dpkg -i lantern.deb
-sudo apt-get install -f
-# echo "----------打开Lantern查看端口------------"
-# echo "----------HTTP端口------------"
-# read HTTPPORT
-# echo "----------SOCKT5端口------------"
-# read SOCKTPORT
-# sudo sed -i 's/^proxy_dns/\#proxy_dns/g' /etc/proxychains.conf
-# sudo sed -i 's/^socks4\s\s127.0.0.1\s9050//g' /etc/proxychains.conf
-# echo "http 127.0.0.1 $HTTPPORT" | sudo tee -a /etc/proxychains.conf
-# echo "sockt5 127.0.0.1 $SOCKTPORT" | sudo tee -a /etc/proxychains.conf
+config_lantern() {
+  echo "---------------lantern---------------"
+  wget https://raw.githubusercontent.com/getlantern/lantern-binaries/master/lantern-installer-beta-64-bit.deb -O lantern.deb
+  sudo dpkg -i lantern.deb
+  sudo apt-get install -f
+  echo "----------打开Lantern查看端口------------"
+  echo "----------HTTP端口------------"
+  read HTTPPORT
+  echo "-------------finished---------------"
+}
+
+config_bash() {
+  echo "---------------bash---------------"
+  echo "alias proxy=\"http_proxy=http://127.0.0.1:$HTTPPORT\"" | tee -a $HOME/.bashrc
+  echo "-------------finished-------------"
+}
 
 #Docker
-curl -sSL https://get.daocloud.io/docker | sh
-sudo usermod -aG docker $USER
-curl -sSL https://get.daocloud.io/daotools/set_mirror.sh | sh -s http://26cc5846.m.daocloud.io
-sudo systemctl restart docker.service
+config_docker() {
+  echo "--------------docker---------------"
+  curl -sSL https://get.daocloud.io/docker | sh
+  sudo usermod -aG docker $USER
+  curl -sSL https://get.daocloud.io/daotools/set_mirror.sh | sh -s http://26cc5846.m.daocloud.io
+  sudo systemctl restart docker.service
+  echo "------------finished---------------"
+}
 
 #MongoDB
-sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6
-echo "deb [ arch=amd64,arm64 ] http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.4.list
-sudo apt-get update
-sudo apt-get install -y mongodb-org
-sudo service mongod start
-echo "sudo service mongod start" | sudo tee -a /etc/rc.local
-
-#配置文件
-cd $HOME
-git clone git@github.com:halsn/ubuntu-config && cd ubuntu-config
-cp -a .bashrc $HOME
-cp -a ./App $HOME
-sudo cp -a ./config/rc.local /etc/
+config_mongo() {
+  echo "---------------mongodb------------------"
+  sudo apt-key adv --keyserver hkp://keyserver.ubuntu.com:80 --recv 0C49F3730359A14518585931BC711F9BA15703C6
+  echo "deb [ arch=amd64,arm64 ] http://repo.mongodb.org/apt/ubuntu xenial/mongodb-org/3.4 multiverse" | sudo tee /etc/apt/sources.list.d/mongodb-org-3.4.list
+  sudo apt-get update
+  sudo apt-get install -y mongodb-org
+  sudo service mongod start
+  echo "sudo service mongod start" | sudo tee -a /etc/rc.local
+  echo "--------------finished-----------------"
+}
 
 # Node
-curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.0/install.sh | bash
-source $HOME/.nvm/nvm.sh
-source $HOME/.profile
-source $HOME/.bashrc
-nvm install stable
-npm install js-beautify eslint_d babel-eslint eslint-config-airbnb eslint-plugin-import eslint-plugin-react eslint-plugin-jsx-a11y htmlhint eslint jsonlint csslint -g
+config_node() {
+  echo "-----------------node------------------"
+  curl -o- https://raw.githubusercontent.com/creationix/nvm/v0.33.0/install.sh | bash
+  source $HOME/.nvm/nvm.sh
+  source $HOME/.profile
+  source $HOME/.bashrc
+  nvm install stable
+  npm install js-beautify eslint_d babel-eslint eslint-config-airbnb eslint-plugin-import eslint-plugin-react eslint-plugin-jsx-a11y htmlhint eslint jsonlint csslint -g
+  echo "---------------finished----------------"
+}
 
 #NVim
-curl -o- https://raw.githubusercontent.com/halsn/neovim-config/master/install.sh | sh
+config_nvim() {
+  echo "-----------------nvim-------------------"
+  curl -o- https://raw.githubusercontent.com/halsn/neovim-config/master/install.sh | sh
+  echo "---------------finished-----------------"
+}
 
 #Robomongo
-proxy wget https://download.robomongo.org/0.9.0/linux/robomongo-0.9.0-linux-x86_64-0786489.tar.gz -O robomonto.tar.gz
-tar -zxf robomongo.tar.gz && cd robomongo
-ROBODIR=$(pwd)
-cd /usr/local/bin/
-sudo ln -s $ROBODIR/bin/robomongo .
+config_robomongo() {
+  echo "------------------robomongo----------------"
+  proxy wget https://download.robomongo.org/0.9.0/linux/robomongo-0.9.0-linux-x86_64-0786489.tar.gz -O robomonto.tar.gz
+  tar -zxf robomongo.tar.gz && cd robomongo
+  ROBODIR=$(pwd)
+  cd /usr/local/bin/
+  sudo ln -s $ROBODIR/bin/robomongo .
+  echo "------------------finished-----------------"
+}
