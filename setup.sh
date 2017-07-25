96e076eddc1fe0ac860aee2b45c0e287b249086e
halsn <xuhalsn@gmail.com>
2017-07-25 01:03:44 -0700

    auto-commit

--- setup.sh
+++ setup.sh
@@ -60,8 +60,9 @@ config_docker() {
 # Docker Compose
 config_docker_compose() {
   echo "---------docker_compose------------"
-  curl https://github.com/docker/compose/releases/download/1.14.0/docker-compose-`uname -s`-`uname -m` > /usr/local/bin/docker-compose
-  chmod +x /usr/local/bin/docker-compose
+  curl https://github.com/docker/compose/releases/download/1.14.0/docker-compose-`uname -s`-`uname -m` > ~/docker-compose
+  chmod +x ~/docker-compose
+  sudo mv ~/docker-compose /usr/local/bin/docker-compose
   curl -L https://raw.githubusercontent.com/docker/compose/master/contrib/completion/bash/docker-compose -o /etc/bash_completion.d/docker-compose
   echo "------------finished---------------"
 }
