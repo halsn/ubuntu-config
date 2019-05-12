563d85ba19eaf16e9d782f9aaa5c65cc8ae06109
halsn <xuhalsn@gmail.com>
2019-05-12 17:31:27 +0800

    auto-commit

--- setup.sh
+++ setup.sh
@@ -4,7 +4,7 @@ sudo apt update
 
 #必备软件
 install_first() {
-  sudo apt-get install -y git ibus-rime ppa-purge tree time curl wget gawk wordnet entr inotify-tools silversearcher-ag htop ncdu
+  sudo apt install -y git ibus-rime ppa-purge tree time curl wget gawk wordnet entr inotify-tools silversearcher-ag htop ncdu
 }
 
 config_ssh() {
